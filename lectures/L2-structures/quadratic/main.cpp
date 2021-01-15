fb87074c4f12df81a769ead92b7ec56ac9f652bc
leonardmohr <64943444+leonardmohr@users.noreply.github.com>
2021-01-15 08:09:11 -0600

    Update main.cpp

--- lectures/L2-structures/quadratic/main.cpp
+++ lectures/L2-structures/quadratic/main.cpp
@@ -29,28 +29,31 @@ struct Solution {
 // solves the quadratic equation using a, b, and c
 // currently: returns true if a solution is found, false otherwise
 // you might want to change that.
-std::pair<std::pair<double, double>, bool> solve_quadratic(int a, int b, int c) {
-    int D = b*b - 4*a*c;
-    if (D >= 0) {
-        double root1 = (-b + sqrt(D))/(2*a);
-        double root2 = (-b - sqrt(D))/(2*a);
-        return {{root1, root2}, true};
-    } else {
-        return {{0, 0}, false};
-    }
+/* 
+ * Will change function so that it returns an std::pair of the form
+ * <does_solution_exist, <sol1, sol2>>;
+ */
+std::pair<std::pair<bool, <sol1, sol2>> solve_quadratic(int a, int b, int c) {
+    double inside = b * b - (4 * a * c);
+    std::pair<double,double> blank;
+    if (inside < 0) return std::make_pair(false, blank);
+    
+    std::pair<double,double> answer = std::make_pair( (-b + sqrt(inside)) / 2,
+                                                      (-b - sqrt(inside)) / 2);
+    return std::make_pair(true, answer);
 }
 
 int main() {
     int a = read_int("Type in a: ");
     int b = read_int("Type in b: ");
     int c = read_int("Type in c: ");
-    auto [roots, found] = solve_quadratic(a, b, c);
-    auto [root1, root2] = roots;
+    auto [found, roots] = solve_quadratic(a, b, c);
     if (found) {
         // if it's a double root (root1 == root2), this is fine as well
+        auto [root1, root2] = roots;
         std::cout << "The roots are: " << root1 << " and " << root2 << std::endl;
     } else {
-        std::cout << "No roots found." << std::endl;
+        std::cout << "No solutions found." << std::endl;
     }
     
 }
